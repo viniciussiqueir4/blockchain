a9870d8e3247d26a3334393a9726395b28fb1071
Marcos Vinicius <vinicius_siqueira-1997@hotmail.com>
2022-09-05 00:44:48 -0300

    fix: prevHash

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ func CreateBlock(data string, prevHash []byte) *Block {
 
 func (chain *BlockChain) AddBlock(data string) {
 	prevBlock := chain.blocks[len(chain.blocks)-1]
-	new := CreateBlock(data, prevBlock.PrevHash)
+	new := CreateBlock(data, prevBlock.Hash)
 	chain.blocks = append(chain.blocks, new)
 }
 
