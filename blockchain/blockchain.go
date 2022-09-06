96a55e15dd35abe3de3f74396cdf765bc2724041
Marcos Vinicius <vinicius_siqueira-1997@hotmail.com>
2022-09-06 12:41:21 -0300

    chore: remove derive hash

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -11,12 +11,6 @@ type Block struct {
 	Nonce    int
 }
 
-// func (b *Block) DeriveHash() {
-// 	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
-// 	hash := sha256.Sum256(info)
-// 	b.Hash = hash[:]
-// }
-
 func CreateBlock(data string, prevHash []byte) *Block {
 	block := &Block{[]byte{}, []byte(data), prevHash, 0}
 	pow := NewProof(block)
